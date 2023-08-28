4a4c6240247a2ebc011c53f7317f225340c7e5a8
Navid Shokri <navid.pdp11@gmail.com>
2023-08-29 01:22:50 +0330

    product service functionality finished

--- eshop/product_service/main.go
+++ eshop/product_service/main.go
@@ -15,8 +15,11 @@ type product struct {
 
 func main(){
 	r := gin.Default();
-	r.GET("/all", getAll);
-    r.POST("/", addProduct);
+	r.GET("/products", getAllProducts);
+    r.POST("/products", addProduct);
+	r.DELETE("/products/:id", deleteProduct);
+	r.GET("/products/:id", getProduct);
+	r.PUT("/products/:id", saleProduct)
 	r.Run("localhost:8080");
 }
 
@@ -24,13 +27,73 @@ var werehouse = []product {};
 
 func addProduct(c *gin.Context){
 	var p product;
-	p.Category = "food";
-	p.Name = "milk";
-	p.Count = 10;
+	if err:= c.BindJSON(&p);err != nil{
+		r := struct	{
+			Success bool;
+			Error string;
+		}{
+			Success: false,
+			Error: err.Error(),
+		};
+		c.IndentedJSON(http.StatusBadRequest, r);
+		return
+	}
 	p.ID = uuid.New()
 	werehouse = append(werehouse, p);
+	r := struct{Success bool}{ Success: true}
+	c.IndentedJSON(http.StatusOK, r);
 }
 
-func getAll(c *gin.Context){
+func getAllProducts(c *gin.Context){
 	c.IndentedJSON(http.StatusOK, werehouse);
+}
+
+func deleteProduct(c *gin.Context){
+	i := c.Param("id");
+	index := getIndexById(i);
+	if index >= 0 {
+		werehouse = removeIndex(werehouse, index);
+		c.IndentedJSON(http.StatusCreated, nil);
+		return;
+	}
+
+	c.IndentedJSON(http.StatusNotFound, nil);
+}
+
+func getProduct(c *gin.Context){
+	id := c.Param("id");
+	index := getIndexById(id);
+	if index >= 0 {
+		c.IndentedJSON(http.StatusOK, werehouse[index]);
+		return;
+	}
+	c.IndentedJSON(http.StatusNotFound, nil);
+}
+
+func saleProduct(c *gin.Context){
+	id := c.Param("id");
+	index := getIndexById(id);
+	if index >= 0 {
+		if werehouse[index].Count > 0 {
+			werehouse[index].Count--;
+			c.IndentedJSON(http.StatusNoContent, nil);
+			return;
+		}
+		c.IndentedJSON(http.StatusBadRequest, nil);
+		return;
+	}
+	c.IndentedJSON(http.StatusNotFound, nil);
+}
+
+func removeIndex(s []product, index int) []product {
+    return append(s[:index], s[index+1:]...)
+}
+
+func getIndexById(id string) int{
+	for t, j := range werehouse {
+		if j.ID == uuid.MustParse(id){
+			return t;
+		}
+	}
+	return -1;
 }
