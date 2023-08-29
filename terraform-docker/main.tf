727bb0d282546c9858f73f9686bc57002f0fd8e0
Navid Shokri <navid.pdp11@gmail.com>
2023-08-29 21:39:10 +0330

    update terraform

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -18,12 +18,32 @@ resource "docker_image" "user_service" {
   #keep_locally = false
 }
 
-resource "docker_container" "my_user_service" {
+resource "docker_container" "iac_user_service" {
   image = docker_image.user_service.name
-  name  = "my_user_service"
+  name  = "iac_user_service"
 
   ports {
     internal = 8000
     external = 8000
+  }
+}
+
+
+resource "docker_image" "product_service" {
+  name = "product_service"
+  build {
+    context    = "../eshop/product_service"
+    dockerfile = "Dockerfile"
+  }
+  #keep_locally = false
+}
+
+resource "docker_container" "iac_product_service" {
+  image = docker_image.product_service.name
+  name  = "iac_product_service"
+
+  ports {
+    internal = 8080
+    external = 8080
   }
 }
