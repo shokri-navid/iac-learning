2560dfd071082ee158f66a89f86c4f9e0c55b9de
Navid Shokri <navid.pdp11@gmail.com>
2023-09-08 21:43:22 +0330

    product service added

--- terraform-k8s/main.tf
+++ terraform-k8s/main.tf
@@ -16,9 +16,9 @@ resource "kubernetes_namespace" "iac-learning" {
   }
 }
 
-resource "kubernetes_service" "ingress" {
+resource "kubernetes_service" "ingress_user" {
   metadata {
-    name      = "ingress"
+    name      = "ingress-user"
     namespace = "ingress"
   }
   spec {
@@ -26,13 +26,37 @@ resource "kubernetes_service" "ingress" {
       name = "nginx-ingress-microk8s"
     }
     port {
+      name = "user-http"
+      protocol    = "TCP"
       port        = 80
       target_port = 8000
     }
+
     type = "LoadBalancer"
   }
 }
 
+resource "kubernetes_service" "ingress_product" {
+  metadata {
+    name      = "ingress-product"
+    namespace = "ingress"
+  }
+  spec {
+    selector = {
+      name = "nginx-ingress-microk8s"
+    }
+    port {
+      name = "user-http"
+      protocol    = "TCP"
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+# user app
 resource "kubernetes_deployment" "user-app" {
   metadata {
     name      = "user-app"
@@ -108,6 +132,90 @@ resource "kubernetes_ingress_v1" "user-ingress" {
               name = "user-service"
               port {
                 number = 8000
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# product app
+resource "kubernetes_deployment" "product-app" {
+  metadata {
+    name      = "product-app"
+    namespace = "iac-learning"
+    labels = {
+      app = "user-app"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "product-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "product-app"
+        }
+      }
+
+      spec {
+        container {
+          image = "localhost:32000/product_service:latest"
+          name  = "product-app"
+          port {
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "product-service" {
+  metadata {
+    name      = "product-service"
+    namespace = "iac-learning"
+  }
+  spec {
+    selector = {
+      app = "product-app"
+    }
+    port {
+      port        = 8080
+      target_port = 8080
+    }
+    type = "NodePort"
+  }
+}
+
+resource "kubernetes_ingress_v1" "product-ingress" {
+  metadata {
+    name      = "product-ingress"
+    namespace = "iac-learning"
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+  spec {
+    rule {
+      host = "product.local"
+      http {
+        path {
+          path = "/"
+          backend {
+            service {
+              name = "product-service"
+              port {
+                number = 8080
               }
             }
           }
